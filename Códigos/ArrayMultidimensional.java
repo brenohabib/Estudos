26fe4b2b8fea01853da95fedf00d51b7576babec
Habs <brenohabib2013@gmail.com>
2023-07-29 12:09:18 -0300

    vault backup: 2023-07-29 12:09:18

--- Códigos/ArrayMultidimensional.java
+++ Códigos/ArrayMultidimensional.java
@@ -6,5 +6,7 @@ class ArrayMultidimensional {
         double[][] larguraAltura = {{2, 3}, {3, 4}};
         double area = larguraAltura[0][0] * larguraAltura[0][1];
         double area2 = larguraAltura[1][0] * larguraAltura[1][1];
+        System.out.println(area);
+        System.out.println(area2);
     }
 }
