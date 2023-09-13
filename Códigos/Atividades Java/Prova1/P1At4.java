14765e2bc0130d9bb6cedd55a790434bef53a0e5
Habs <brenohabib2013@gmail.com>
2023-09-13 19:09:54 -0300

    testing

--- Códigos/Atividades Java/Prova1/P1At4.java
+++ Códigos/Atividades Java/Prova1/P1At4.java
@@ -1,12 +1,13 @@
 public class P1At4 {
     public static void main(String[] args) {
-        String[][]matriz = new String[15][15];
-        int a = 7;
-        int b = 7;
-        int r = 8;
+        String[][]matriz = new String[21][21];
+        int a = 10;
+        int b = 10;
+        int r = 7;
         for(int y = 0; y < matriz.length; y++){
             for(int x = 0; x < matriz[y].length; x++){
-                if(((x^2 + y^2) - ((2*a*x) - (2*b*y)) + (a^2 + b^2 - r^2)) <= 0){
+                //if(((x^2 + y^2) - ((2*a*x) - (2*b*y)) + (a^2 + b^2 - r^2)) <= 0){
+                if(x*x + y*y - 2*a*x - 2*b*y + a*a + b*b - r*r <=0){  
                     matriz[x][y] = "*";
                 }
                 else{
