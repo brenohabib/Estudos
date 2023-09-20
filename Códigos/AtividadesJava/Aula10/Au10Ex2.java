09267b1cf9147259065942826c3e7256119717a5
Habs <brenohabib2013@gmail.com>
2023-09-20 14:35:03 -0300

    Au10

---
+++ Códigos/AtividadesJava/Aula10/Au10Ex2.java
@@ -0,0 +1,70 @@
+package AtividadesJava.Aula10;
+
+public class Au10Ex2 {
+    public static void main(String[] args) {
+
+        int[] array = new int[10];
+        boolean isDescending = true;
+
+        array = randomArray(array);
+        System.out.print("Antes: ");
+        printArray(array);
+
+        array = selectionSort(array, isDescending);
+        System.out.print("Depois:");
+        printArray(array);
+
+    }
+
+    public static int[] selectionSort(int[] array, boolean isDescending) {
+        int key, i, j;
+        if (isDescending) {
+            for (i = 1; i < array.length; i++) {
+                key = array[i];
+                j = i - 1;
+
+                while (j >= 0 && key > array[j]) {
+                    array[j + 1] = array[j];
+                    j--;
+
+                }
+                array[j + 1] = key;
+            }
+            return array;
+
+        } else {
+            for (i = 1; i < array.length; i++) {
+                key = array[i];
+                j = i - 1;
+
+                while (j >= 0 && key < array[j]) {
+                    array[j + 1] = array[j];
+                    j--;
+
+                }
+                array[j + 1] = key;
+            }
+            return array;
+        }
+    }
+
+    public static int[] randomArray(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+
+            array[i] = (int) (Math.random() * 11);
+
+        }
+        return array;
+    }
+
+    public static void printArray(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+
+            System.out.print(" | " + array[i]);
+
+        }
+        System.out.println(" | ");
+    }
+}
--- Códigos/AtividadesJava/Aula10/Au10Ex3.java
+++ Códigos/AtividadesJava/Aula10/Au10Ex3.java
@@ -1,11 +1,10 @@
 package AtividadesJava.Aula10;
 
-public class Au10Ex1 {
+public class Au10Ex3 {
     public static void main(String[] args) {
 
-        int[] array = new int[10];
+        String[] array = { "Ana", "Breno", "Maria", "Emily", "Isaac" };
 
-        array = randomArray(array);
         System.out.print("Antes: ");
         printArray(array);
 
@@ -15,34 +14,27 @@ public class Au10Ex1 {
 
     }
 
-    public static int[] selectionSort(int[] array) {
-        int key, i, j;
+    public static String[] selectionSort(String[] array) {
+
+        String key;
+        int i, j;
 
         for (i = 1; i < array.length; i++) {
+
             key = array[i];
             j = i - 1;
 
-            while (j >= 0 && key < array[j]) {
+            while (j >= 0 && key.compareTo(array[j]) < 0) {
                 array[j + 1] = array[j];
-                j--;
-
+                j = j - 1;
             }
             array[j + 1] = key;
         }
+
         return array;
     }
 
-    public static int[] randomArray(int[] array) {
-
-        for (int i = 0; i < array.length; i++) {
-
-            array[i] = (int) (Math.random() * 11);
-
-        }
-        return array;
-    }
-
-    public static void printArray(int[] array) {
+    public static void printArray(String[] array) {
 
         for (int i = 0; i < array.length; i++) {
 
