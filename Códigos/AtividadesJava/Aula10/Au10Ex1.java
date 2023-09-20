ba5f1b2c1d4fd70fcbca0c91668c0fbab3e23ed7
Habs <brenohabib2013@gmail.com>
2023-09-19 23:17:38 -0300

    vault backup: 2023-09-19 23:17:37

--- Códigos/AtividadesJava/Aula10/Au10Ex1.java
+++ Códigos/AtividadesJava/Aula10/Au10Ex1.java
@@ -3,7 +3,13 @@ package AtividadesJava.Aula10;
 public class Au10Ex1 {
     public static void main(String[] args) {
         
-        
 
+
+    }
+    public static void selection(int[] vetor){
+        
+        for(int i = 1; i < vetor.length){
+            
+        }
     }
 }
