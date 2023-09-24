8cd472bdd130bc4fc26457b583d8afa5d96c03dc
Habs <brenohabib2013@gmail.com>
2023-09-24 13:17:20 -0300

    vault backup: 2023-09-24 13:17:20

---
+++ Chess/src/Piece.java
@@ -0,0 +1,8 @@
+
+public abstract class Piece {
+	
+	public abstract String name();
+	public abstract void move();
+	public abstract boolean inBoard();
+	
+}
---
+++ Chess/src/root.java
@@ -0,0 +1,6 @@
+
+public class root{
+	public static void main (String[] args) {
+		
+	}
+}
