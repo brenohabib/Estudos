585397efbea771b1afccae3da37b88d5374e2806
Habs <brenohabib2013@gmail.com>
2023-09-25 13:13:21 -0300

    deleted chess game I've downloaded

--- Chess/src/Piece.java
+++
@@ -1,8 +0,0 @@
-
-public abstract class Piece {
-	
-	public abstract String name();
-	public abstract void move();
-	public abstract boolean inBoard();
-	
-}
--- Chess/src/root.java
+++
@@ -1,6 +0,0 @@
-
-public class root{
-	public static void main (String[] args) {
-		
-	}
-}
