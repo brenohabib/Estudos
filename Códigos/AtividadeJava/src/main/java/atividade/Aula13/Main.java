02330116586fca1a33be04ef695478b803b2e5cb
Habs <brenohabib2013@gmail.com>
2023-10-08 12:18:27 -0300

    v0.9

--- Códigos/AtividadeJava/src/main/java/atividade/Aula13/Main.java
+++ Códigos/AtividadeJava/src/main/java/atividade/Aula13/Main.java
@@ -11,9 +11,6 @@ import java.util.concurrent.TimeUnit;
 b. Implementar um procedimento para exibir a lista de produtos.
 c. Implementar uma função para buscar um produto pelo nome e retornar seu preço.
 
-3. Ordenação:
-b. Permitir ao usuário escolher se deseja ordenar por nome ou por preço.
-
 5. Funcionalidades:
 b. Implementar uma função que calcule e retorne a média de preço dos produtos
 inseridos.
@@ -40,7 +37,7 @@ public class Main {
         Scanner stringInput = new Scanner(System.in);
 
         String[] nameList = new String[maxProducts];
-        
+
         Float[][] productList = new Float[2][maxProducts];
         int productQuantity = 0;
 
@@ -751,185 +748,222 @@ class Menu {
         return command;
     }
 }
+
 class Sort {
+    
+    public static int[] indices;
 
-    public static Float[] selectionSort(Float[] array, boolean isAscending) {
-
-        for (int i = 0; i < array.length - 1; i++) {
+    public static void selectionSort(String[] array, Float[][] products, boolean isAscending) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
             int key = i;
-            for (int j = i; j < array.length; j++) {
-                if(array[j] != null){
-                    if (isAscending && array[j] < array[key]) {
-                        key = j;
-                    }
-
-                    else if (!isAscending && array[j] > array[key]) {
+            for (int j = i; j < n; j++) {
+                if (array[j] != null && array[key] != null) {
+                    if ((isAscending && array[j].compareTo(array[key]) < 0) || (!isAscending && array[j].compareTo(array[key]) > 0)) {
                         key = j;
                     }
                 }
             }
-            Float temp = array[key];
-            array[key] = array[i];
-            array[i] = temp;
-        }
-        return array;
-    }
+            if (array[key] != null && products[0][key] != null && products[1][key] != null) {
+                String tempName = array[key];
+                array[key] = array[i];
+                array[i] = tempName;
 
-    public static String[] selectionSort(String[] array, boolean isAscending) {
-        for (int i = 0; i < array.length - 1; i++) {
+                Float tempPrice = products[0][key];
+                Float tempQuantity = products[1][key];
+
+                products[0][key] = products[0][i];
+                products[1][key] = products[1][i];
+
+                products[0][i] = tempPrice;
+                products[1][i] = tempQuantity;
+            }
+        }
+    }
+    
+    public static void selectionSort(Float[][] array, String[] names, boolean isAscending) {
+        int n = array[0].length;
+        for (int i = 0; i < n - 1; i++) {
             int key = i;
-            for (int j = i; j < array.length; j++) {
-                if(array[j] != null){
-                    if (isAscending && array[j].compareTo(array[key]) < 0) {
-                        key = j;
-                    } else if (!isAscending && array[j].compareTo(array[key]) > 0) {
+            for (int j = i; j < n; j++) {
+                if (array[0][j] != null && array[0][key] != null) {
+                    if ((isAscending && array[0][j] < array[0][key]) || (!isAscending && array[0][j] > array[0][key])) {
                         key = j;
                     }
                 }
             }
-            String temp = array[key];
-            array[key] = array[i];
-            array[i] = temp;
-        }
-        return array;
-    }
+            if (array[0][key] != null && array[1][key] != null) {
+                Float tempPrice = array[0][key];
+                Float tempQuantity = array[1][key];
+                String tempName = names[key];
 
-    public static Float[] insertionSort(Float[] array, boolean isAscending) {
-        Float key;
-        int i, j;
-        for (i = 1; i < array.length; i++) {
-            key = array[i];
-            j = i - 1;
+                array[0][key] = array[0][i];
+                array[1][key] = array[1][i];
+                names[key] = names[i];
 
-            if(array[j] != null){
-                while (j >= 0 && (isAscending && key < array[j])) {
-                    array[j + 1] = array[j];
-                    j--;
-                }
-
-                while (j >= 0 && (!isAscending && key > array[j])) {
-                    array[j + 1] = array[j];
-                    j--;
-                }
+                array[0][i] = tempPrice;
+                array[1][i] = tempQuantity;
+                names[i] = tempName;
             }
-            array[j + 1] = key;
         }
-        return array;
     }
+    
 
-    public static String[] insertionSort(String[] array, boolean isAscending) {
-        
+    public static void insertionSort(String[] array, Float[][] products, boolean isAscending) {
         String key;
         int i, j;
 
         for (i = 1; i < array.length; i++) {
-                key = array[i];
-                j = i - 1;
+            if (array[i] == null) continue;
+            key = array[i];
+            Float productKey1 = products[0][i];
+            Float productKey2 = products[1][i];
 
-                while (j >= 0 && (isAscending && key.compareTo(array[j]) < 0) && array[j] != null) {
-                    array[j + 1] = array[j];
+            j = i - 1;
+
+            while (j >= 0) {
+                if (array[j] == null) {
                     j--;
+                    continue;
                 }
 
-                while (j >= 0 && (!isAscending && key.compareTo(array[j]) > 0) && array[j] != null) {
+                if ((isAscending && key.compareTo(array[j]) < 0) || (!isAscending && key.compareTo(array[j]) > 0)) {
                     array[j + 1] = array[j];
+                    products[0][j + 1] = products[0][j];
+                    products[1][j + 1] = products[1][j];
                     j--;
+                } else {
+                    break;
                 }
+            }
 
-                array[j + 1] = key;
+            array[j + 1] = key;
+            products[0][j + 1] = productKey1;
+            products[1][j + 1] = productKey2;
         }
-        return array;
     }
 
-    public static Float[] bubbleSort(Float[] array, boolean isAscending) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - 1; j++) {
 
-                if (array[j] != null && array[j + 1] != null) {
-                    if ((isAscending && array[j] > array[j + 1]) || (!isAscending && array[j] < array[j + 1])) {
-                        float temp = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = temp;
+    public static void insertionSort(Float[][] array, String[] names, boolean isAscending) {
+        int i, j;
+
+        for (i = 1; i < array[0].length; i++) {
+            if (array[0][i] == null) continue;
+            Float keyPrice = array[0][i];
+            Float keyQuantity = array[1][i];
+            String keyName = names[i];
+            j = i - 1;
+
+            while (j >= 0) {
+                if (array[0][j] == null) {
+                    j--;
+                    continue;
+                }
+
+                if ((isAscending && keyPrice < array[0][j]) || (!isAscending && keyPrice > array[0][j])) {
+                    array[0][j + 1] = array[0][j];
+                    array[1][j + 1] = array[1][j];
+                    names[j + 1] = names[j];
+                    j--;
+                } else {
+                    break;
+                }
+            }
+
+            array[0][j + 1] = keyPrice;
+            array[1][j + 1] = keyQuantity;
+            names[j + 1] = keyName;
+        }
+    }
+
+    public static void bubbleSort(Float[][] array, String[] names, boolean isAscending) {
+        int n = array[0].length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+
+                if (array[0][j] != null && array[0][j + 1] != null) {
+                    if ((isAscending && array[0][j] > array[0][j + 1])
+                            || (!isAscending && array[0][j] < array[0][j + 1])) {
+
+                        Float tempPrice = array[0][j];
+                        Float tempQuantity = array[1][j];
+                        String tempName = names[j];
+
+                        array[0][j] = array[0][j + 1];
+                        array[1][j] = array[1][j + 1];
+                        names[j] = names[j + 1];
+
+                        array[0][j + 1] = tempPrice;
+                        array[1][j + 1] = tempQuantity;
+                        names[j + 1] = tempName;
                     }
                 }
             }
         }
-        return array;
     }
 
-    public static String[] bubbleSort(String[] array, boolean isAscending) {
+    public static void bubbleSort(String[] array, Float[][] products, boolean isAscending) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
 
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = 0; j < array.length - i - 1; j++) {
-                
                 if (array[j] != null && array[j + 1] != null) {
-                    if (array[j].compareTo(array[j + 1]) > 0) {
+                    if ((isAscending && array[j].compareTo(array[j + 1]) > 0)
+                            || (!isAscending && array[j + 1].compareTo(array[j]) > 0)) {
+
                         String temp = array[j];
                         array[j] = array[j + 1];
                         array[j + 1] = temp;
+
+                        Float[] tempProduct = new Float[2];
+                        tempProduct[0] = products[0][j];
+                        tempProduct[1] = products[1][j];
+
+                        products[0][j] = products[0][j + 1];
+                        products[1][j] = products[1][j + 1];
+
+                        products[0][j + 1] = tempProduct[0];
+                        products[1][j + 1] = tempProduct[1];
                     }
                 }
             }
         }
-        return array;
     }
     
     public static void chooseSort(String[] data, String[] nameList, Float[][] productList) {
 
-        boolean ascending = true;
+        int n = nameList.length;
+        indices = new int[n];
 
-        if (data[2].equals("Crescente")) {
-            ascending = true;
+        for (int i = 0; i < n; i++) {
+            indices[i] = i;
+
         }
-        if (data[2].equals("Decrescente")) {
-            ascending = false;
-        }
-        
+        boolean ascending = data[2].equals("Crescente");
 
         if (data[0].equals("Nome")) {
 
             if (data[1].equals("Bubble Sort")) {
+                Sort.bubbleSort(nameList, productList, ascending);
 
-                Sort.bubbleSort(nameList, ascending);
+            } else if (data[1].equals("Insertion Sort")) {
+                Sort.insertionSort(nameList, productList, ascending);
 
+            } else if (data[1].equals("Selection Sort")) {
+                Sort.selectionSort(nameList, productList, ascending);
             }
 
-            if (data[1].equals("Insertion Sort")) {
+        } 
+        else if (data[0].equals("Preço")) {
 
-                Sort.insertionSort(nameList, ascending);
-
-            }
-
-            if (data[1].equals("Selection Sort")) {
-
-                Sort.selectionSort(nameList, ascending);
-
-            }
-
-        }
-        
-        if (data[0].equals("Preço")) {
-            
             if (data[1].equals("Bubble Sort")) {
+                Sort.bubbleSort(productList, nameList, ascending);
 
-                Sort.bubbleSort(productList[0], ascending);
-                Sort.bubbleSort(productList[1], ascending);
+            } else if (data[1].equals("Insertion Sort")) {
+                Sort.insertionSort(productList, nameList, ascending);
 
-            }
-
-            if (data[1].equals("Insertion Sort")) {
-
-                Sort.insertionSort(productList[0], ascending);
-                Sort.insertionSort(productList[1], ascending);
-
-            }
-
-            if (data[1].equals("Selection Sort")) {
-
-                Sort.selectionSort(productList[0], ascending);
-                Sort.selectionSort(productList[1], ascending);
-
-
+            } else if (data[1].equals("Selection Sort")) {
+                Sort.selectionSort(productList, nameList, ascending);
             }
         }
     }
