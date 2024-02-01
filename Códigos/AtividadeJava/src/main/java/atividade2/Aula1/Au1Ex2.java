1f3e52e968828845eee2216440562a3f256ea85b
brenohabib <brenohabib2013@gmail.com>
2024-02-01 12:42:06 -0300

    vault backup: 2024-02-01 12:42:05

--- Códigos/AtividadeJava/src/main/java/atividade2/Aula1/Au1Ex2.java
+++ Códigos/AtividadeJava/src/main/java/atividade2/Aula1/Au1Ex2.java
@@ -2,6 +2,16 @@ package atividade2.Aula1;
 
 public class Au1Ex2 {
     public static void main(String[] args) {
-        
+        System.out.println(fatorial(5));
     }
+
+    public static int fatorial(int valor) {
+        int fatorial = valor;
+        for (int i = 0; i <= valor; i++) {
+            valor--;
+            fatorial *= valor;
+        }
+        return fatorial;
+    }
+
 }
