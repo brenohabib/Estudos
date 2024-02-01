06c757c8e6acc71a4ba04c47ad2e8eb1566dd25d
brenohabib <brenohabib2013@gmail.com>
2024-02-01 13:24:13 -0300

    vault backup: 2024-02-01 13:24:13

--- Códigos/AtividadeJava/src/main/java/atividade2/Aula1/Au1Ex2.java
+++ Códigos/AtividadeJava/src/main/java/atividade2/Aula1/Au1Ex2.java
@@ -1,8 +1,12 @@
 package atividade2.Aula1;
 
+import java.util.Scanner;
+
 public class Au1Ex2 {
     public static void main(String[] args) {
-        System.out.println(fatorial(5));
+        Scanner input = new Scanner(System.in);
+        System.out.print("Digite um valor: ");
+        System.out.println("Fatorial = " + fatorial(input.nextInt()));
     }
 
     public static int fatorial(int valor) {
