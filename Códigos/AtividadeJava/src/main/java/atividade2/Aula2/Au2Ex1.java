e1254241bdecbc43fd30859f1c94b45bfa4dec5e
brenohabib <brenohabib2013@gmail.com>
2024-02-02 21:10:19 -0300

    vault backup: 2024-02-02 21:10:19

--- Códigos/AtividadeJava/src/main/java/atividade2/Aula2/Au2Ex1.java
+++ Códigos/AtividadeJava/src/main/java/atividade2/Aula2/Au2Ex1.java
@@ -2,11 +2,20 @@ package atividade2.Aula2;
 
 public class Au2Ex1 {
     public static void main(String[] args) {
-        
+        parimpar(4);
     }
 
-    public static void parimpar(){
-        
+    public static void parimpar(int num) {
+        if(num < 0) {
+            System.out.println("O numero deve ser positivo");
+        }
+        else {
+            if (num % 2 == 0) {
+                System.out.println("O número é par!");
+            }
+            else {
+                System.out.println("O numero é impar!");
+            }
+        }
     }
-
 }
