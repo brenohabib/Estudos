2242402061cf36a2b66240dfb1290e238769a586
brenohabib <brenohabib2013@gmail.com>
2024-02-07 19:47:59 -0300

    vault backup: 2024-02-07 19:47:58

--- Códigos/AtividadeJava/src/main/java/atividade2/Aula2/Au2Ex4.java
+++ Códigos/AtividadeJava/src/main/java/atividade2/Aula2/Au2Ex4.java
@@ -2,7 +2,7 @@ package atividade2.Aula2;
 
 public class Au2Ex4 {
     public static void main(String[] args) {
-        System.out.println(calcularPotencia(5, 3));
+        System.out.println(calcularPotencia(5, -2));
     }
     public static int calcularPotencia(int base, int expoente) {
         return (int)(Math.pow((double)base, (double)expoente));
