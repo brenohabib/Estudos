7cf3405a62d83c502f665d48cede2f5f19b6da70
brenohabib <brenohabib2013@gmail.com>
2024-02-09 21:25:16 -0300

    vault backup: 2024-02-09 21:25:16

---
+++ Códigos/AtividadeJava/src/main/java/atividade2/Aula3/Au3Ex1.java
@@ -0,0 +1,11 @@
+package atividade2.Aula3;
+
+public class Au3Ex1 {
+    public static int dobro(int valor){
+        valor *= 2;
+        return valor;
+    }
+    public static void main(String[] args) {
+        System.out.println(dobro(5));
+    }
+}
---
+++ Códigos/AtividadeJava/src/main/java/atividade2/Aula3/Au3Ex2.java
@@ -0,0 +1,10 @@
+package atividade2.Aula3;
+
+public class Au3Ex2 {
+    public static void main(String[] args) {
+        
+    }
+    public static int encontrarPrimosIntervalo(int inicio, int fim){
+        
+    }
+}
