b0ba56c18503d5bda685d69a0996ad78425bb2d4
brenohabib <brenohabib2013@gmail.com>
2024-02-15 12:09:58 -0300

    vault backup: 2024-02-15 12:09:57

--- Códigos/AtividadeJava/src/main/java/atividade2/Aula3/Au3Ex4.java
+++ Códigos/AtividadeJava/src/main/java/atividade2/Aula3/Au3Ex4.java
@@ -2,12 +2,18 @@ package atividade2.Aula3;
 
 public class Au3Ex4 {
     public static void main(String[] args) {
-        
+        System.out.println(vetificarPalindromo("ovo"));
     }
     public static boolean vetificarPalindromo(String palavra) {
-        String aux = palavra;
-        for(int i = 0; i < palavra.length(); i++){
-            
+        String aux = "";
+        for(int i = palavra.length()-1; i >= 0; i--){
+            aux += palavra.charAt(i);
+        }
+        if (palavra.equals(aux)){
+            return true;
+        }
+        else{
+            return false;
         }
     }
 }
