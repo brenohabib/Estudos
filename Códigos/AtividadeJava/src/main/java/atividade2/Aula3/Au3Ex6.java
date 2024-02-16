30a1115cb5485cfe95b9b9200a074718f316019d
brenohabib <brenohabib2013@gmail.com>
2024-02-16 12:42:15 -0300

    vault backup: 2024-02-16 12:42:15

--- Códigos/AtividadeJava/src/main/java/atividade2/Aula3/Au3Ex6.java
+++ Códigos/AtividadeJava/src/main/java/atividade2/Aula3/Au3Ex6.java
@@ -2,15 +2,15 @@ package atividade2.Aula3;
 
 public class Au3Ex6 {
     public static void main(String[] args) {
-        int[][] matrix = {{1, 2, 1}, {4, 5, 1}, {1,1,1}};
-        printMatrix(matrix, 0); // Use the default padding of 0
+        int[][] matrix = {{10, 2, 1}, {4, 5, 1}, {1,1,1}};
+        printMatrix(matrix);
     }
     
-    public static void printMatrix(int[][] matrix, int padding) {
+    public static void printMatrix(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 String number = String.valueOf(matrix[i][j]);
-                String formattedNumber = String.format("%-" + (padding + number.length()) + "s", number); // Efficient centering
+                String formattedNumber = String.format("%-" + (number.length()) + "s", number);
                 System.out.printf("| %s ", formattedNumber);
             }
             System.out.println("|");
