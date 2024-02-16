fd2fe09c2784dab05f82f213fbdf0389ae3b6c57
brenohabib <brenohabib2013@gmail.com>
2024-02-16 12:26:47 -0300

    vault backup: 2024-02-16 12:26:47

--- Códigos/AtividadeJava/src/main/java/atividade2/Aula3/Au3Ex6.java
+++ Códigos/AtividadeJava/src/main/java/atividade2/Aula3/Au3Ex6.java
@@ -2,46 +2,19 @@ package atividade2.Aula3;
 
 public class Au3Ex6 {
     public static void main(String[] args) {
-        int[][] matrix = {{1,2}, {4,5}};
-        printMatrix(matrix);
+        int[][] matrix = {{1, 2, 1}, {4, 5, 1}, {1,1,1}};
+        printMatrix(matrix, 0); // Use the default padding of 0
     }
-    public static void printMatrix(int[][] matrix) {
-
-        int maxLength = String.valueOf(matrix[matrix.length - 1][matrix[0].length - 1]).length();
-        System.out.print(" ");
-        for (int i = 0; i < (maxLength * matrix.length) + matrix.length * 3 + 1; i++) {
-            System.out.print("-");
-        }
-        System.out.println();
+    
+    public static void printMatrix(int[][] matrix, int padding) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-
                 String number = String.valueOf(matrix[i][j]);
-                int padding = (maxLength - number.length()) / 2;
-                System.out.printf(" | %" + maxLength + "s", centralizeString(number, padding));
+                String formattedNumber = String.format("%-" + (padding + number.length()) + "s", number); // Efficient centering
+                System.out.printf("| %s ", formattedNumber);
             }
-            System.out.println(" | ");
-            System.out.print(" ");
-            for (int j = 0; j < (maxLength * matrix.length) + matrix.length * 3 + 1; j++) {
-                System.out.print("-");
-            }
-            System.out.println();
+            System.out.println("|");
         }
-    }
-
-    public static String centralizeString(String input, int padding) {
-
-        StringBuilder result = new StringBuilder(input.length() + 2 * padding);
-        for (int i = 0; i < padding; i++) {
-
-            result.append(" ");
-        }
-        result.append(input);
-
-        for (int i = 0; i < padding; i++) {
-
-            result.append(" ");
-        }
-        return result.toString();
+        System.out.println();
     }
 }
