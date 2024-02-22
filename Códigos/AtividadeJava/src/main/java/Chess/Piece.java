ff8a32faff203d4614c59413b3ca9164c4d445b7
brenohabib <brenohabib2013@gmail.com>
2024-02-22 10:55:13 -0300

    vault backup: 2024-02-22 10:55:13

---
+++ Códigos/AtividadeJava/src/main/java/Chess/Piece.java
@@ -0,0 +1,10 @@
+package Chess;
+
+public abstract class Piece {
+    private int posX;
+    private int posY;
+
+    public void move(){
+        
+    }
+}
--- Códigos/AtividadeJava/src/main/java/Game/Game.java
+++
@@ -1,7 +0,0 @@
-package Game;
-public class Game {
-    public static void main(String[] args) {
-        Entity player = new Entity();
-        System.out.println(player.getArmor());
-    }
-}
