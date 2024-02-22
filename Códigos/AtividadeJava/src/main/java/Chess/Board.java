a72bf776ecbb09857432d2a7deac021f8941d438
brenohabib <brenohabib2013@gmail.com>
2024-02-22 11:12:19 -0300

    vault backup: 2024-02-22 11:12:19

--- Códigos/AtividadeJava/src/main/java/Chess/Board.java
+++ Códigos/AtividadeJava/src/main/java/Chess/Board.java
@@ -1,20 +1,20 @@
 package Chess;
 
 public class Board {
-    private int[][] board = new int[8][8];
+    private Piece[][] board = new Piece[8][8];
 
-    public int[][] getBoard() {
+    public Piece[][] getBoard() {
         return board;
     }
 
-    public void setBoard(int[][] board) {
-        this.board = board;
+    public void addPieceAt(int x, int y, Piece piece) {
+        this.board[x][y] = piece;
     }
 
     public void init(){
         for(int i = 0; i < board.length; i++) {
-            setBoard(this.board[i][1] = new Pawn());
-
+            addPieceAt(i, 1, new Pawn());
+            addPieceAt(i, 6, new Pawn());
         }
     }
 }
