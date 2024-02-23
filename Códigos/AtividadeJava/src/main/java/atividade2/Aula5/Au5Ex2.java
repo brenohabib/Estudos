09490b9fb9792e5b3e9afa87261902051163cf22
brenohabib <brenohabib2013@gmail.com>
2024-02-23 13:21:11 -0300

    vault backup: 2024-02-23 13:21:11

---
+++ Códigos/AtividadeJava/src/main/java/atividade2/Aula5/Au5Ex2.java
@@ -0,0 +1,13 @@
+package atividade2.Aula5;
+
+public class Au5Ex2 {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int[] somaVetor(int[] vetor) {
+        
+
+        return vetor;
+    }
+}
