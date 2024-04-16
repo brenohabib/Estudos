edb68f3c96637a58bbf3d3e1dda72cf79d29d9b3
brenohabib <brenohabib2013@gmail.com>
2024-04-16 20:15:08 -0300

    vault backup: 2024-04-16 20:15:08

---
+++ Códigos/AtividadeJava/src/main/java/atividade2/Aula12/Calculadora.java
@@ -0,0 +1,33 @@
+package atividade2.Aula12;
+
+public class Calculadora {
+    private double numero1;
+    private double numero2;
+
+    public Calculadora(double numero1, double numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+    public double dividir() {
+        return getNumero1() / getNumero2();
+    }
+
+    public double getNumero1() {
+        return numero1;
+    }
+    public void setNumero1(double numero1) {
+        this.numero1 = numero1;
+    }
+    public double getNumero2() {
+        return numero2;
+    }
+    public void setNumero2(double numero2) {
+        this.numero2 = numero2;
+    }
+
+    class DivisaoPorZeroExeption extends Exception {
+        public DivisaoPorZeroExeption(String message) {
+            super(message);
+        }
+    }
+}
---
+++ Códigos/AtividadeJava/src/main/java/atividade2/Aula12/MainCalculadora.java
@@ -0,0 +1,8 @@
+package atividade2.Aula12;
+
+public class MainCalculadora {
+    public static void main(String[] args) {
+        Calculadora teste = new Calculadora(2, 0);
+        System.out.println(teste.dividir());
+    }
+}
