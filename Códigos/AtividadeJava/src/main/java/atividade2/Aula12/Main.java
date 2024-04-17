041181d9e11caab4422faef45f78cf8f6526f90b
brenohabib <brenohabib2013@gmail.com>
2024-04-17 08:13:40 -0300

    vault backup: 2024-04-17 08:13:40

---
+++ Códigos/AtividadeJava/src/main/java/atividade2/Aula12/Main.java
@@ -0,0 +1,20 @@
+package atividade2.Aula12;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Digite o numerador: ");
+        int numerador = input.nextInt();
+        System.out.print("Digite o denomidador: ");
+        int denominador = input.nextInt();
+        try {
+            double resultado = Calculadora.dividir(numerador, denominador);
+            System.out.println("Resultado: " + resultado);
+        } catch (DivisaoPorZeroException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+        input.close();
+    }
+}
--- Códigos/AtividadeJava/src/main/java/atividade2/Aula12/MainCalculadora.java
+++
@@ -1,8 +0,0 @@
-package atividade2.Aula12;
-
-public class MainCalculadora {
-    public static void main(String[] args) {
-        Calculadora teste = new Calculadora(2, 0);
-        System.out.println(teste.dividir());
-    }
-}
