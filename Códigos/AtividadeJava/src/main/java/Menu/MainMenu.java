858f0eea8871ae4d676237a0e7b6a28520de1550
brenohabib <brenohabib2013@gmail.com>
2024-04-25 07:45:08 -0300

    commit

--- Códigos/AtividadeJava/src/main/java/Menu/MainMenu.java
+++ Códigos/AtividadeJava/src/main/java/Menu/MainMenu.java
@@ -2,14 +2,9 @@ package Menu;
 
 public class MainMenu {
     public static void main(String[] args) {
-        String text;
-        Menu mainMenu = new Menu("Teste", 80);
-        mainMenu.clearScreen();
-        mainMenu.printTitle();
-        mainMenu.printText("texto", Alignment.LEFT);
-        mainMenu.printLine();
-        text = mainMenu.input("input: ");
-        mainMenu.clearScreen();
-        System.out.println(text);
-    }   
+        Menu mainMenu = new Menu("Vending Machine", 80);
+        mainMenu.start();
+        mainMenu.text("Credit: R$20.00", 1, Alignment.RIGHT);
+        mainMenu.input();
+    }
 }
