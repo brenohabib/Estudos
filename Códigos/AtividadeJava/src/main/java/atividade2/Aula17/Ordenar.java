03ba1d37fc8592e2242f2632a7d359c0945088aa
brenohabib <brenohabib2013@gmail.com>
2024-05-16 21:27:57 -0300

    vault backup: 2024-05-16 21:27:57

---
+++ Códigos/AtividadeJava/src/main/java/atividade2/Aula17/Ordenar.java
@@ -0,0 +1,44 @@
+package atividade2.Aula17;
+
+import java.io.*;
+import java.util.*;
+import atividade2.Menu;
+
+public class Ordenar {
+    public static void main(String[] args) {
+        Menu mainMenu = new Menu("Ordenador", 80);
+        mainMenu.start();
+        ordenarNumeros();
+        mainMenu.text("Texto ordenado com sucesso!", 4);
+    }
+
+    public static void ordenarNumeros() {
+        try {
+            File arquivo = new File("numeros.txt");
+            FileReader leitor = new FileReader(arquivo);
+            BufferedReader bufferLeitura = new BufferedReader(leitor);
+            String linha = bufferLeitura.readLine();
+            leitor.close();
+
+            String[] numerosString = linha.split(",");
+            Integer[] numeros = new Integer[numerosString.length];
+            for (int i = 0; i < numerosString.length; i++) {
+                numeros[i] = Integer.parseInt(numerosString[i]);
+            }
+
+            Arrays.sort(numeros, Collections.reverseOrder());
+
+            FileWriter escritor = new FileWriter(arquivo);
+            BufferedWriter bufferEscrita = new BufferedWriter(escritor);
+            for (int i = 0; i < numeros.length; i++) {
+                bufferEscrita.write(numeros[i].toString());
+                if (i != numeros.length - 1) {
+                    bufferEscrita.write(",");
+                }
+            }
+            bufferEscrita.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- Códigos/AtividadeJava/src/main/java/atividade2/Aula17/Unificar.java
+++ Códigos/AtividadeJava/src/main/java/atividade2/Aula17/Unificar.java
@@ -2,6 +2,7 @@ package atividade2.Aula17;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 
@@ -11,9 +12,63 @@ import java.io.FileNotFoundException;
 import atividade2.Menu;
 
 public class Unificar {
+    static Menu fileMenu = new Menu("Unificador", 80);
+
     public static void main(String[] args) {
-        Menu mainMenu = new Menu("Unificador", 80);
+
+        fileMenu.start();
+
+        try {
+            FileReader texto1 = new FileReader("texto1.txt");
+            BufferedReader leitor1 = new BufferedReader(texto1);
+
+            FileReader texto2 = new FileReader("texto2.txt");
+            BufferedReader leitor2 = new BufferedReader(texto2);
+
+            File concatenado = new File("concatenado.txt");
+            FileWriter escritor = new FileWriter(concatenado);
+            BufferedWriter bufferEscritor = new BufferedWriter(escritor);
+
+            if (!concatenado.exists()) {
+                criarArquivo("concatenado.txt");
+            }
+
+            String linha1, linha2;
+            while ((linha1 = leitor1.readLine()) != null && (linha2 = leitor2.readLine()) != null) {
+                bufferEscritor.write(linha1);
+                bufferEscritor.newLine();
+                bufferEscritor.write(linha2);
+                bufferEscritor.newLine();
+            }
+
+            bufferEscritor.close();
+            escritor.close();
+            leitor1.close();
+            leitor2.close();
+
+        } catch (FileNotFoundException e) {
+            System.err.println("Não foi possível achar o arquivo: ");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Não foi possível alterar o arquivo: ");
+            e.printStackTrace();
+        }
+
+        fileMenu.text("Arquivo concatenado com sucesso!", 4);
     }
 
-    
+    private static void criarArquivo(String nome) {
+        try {
+            File arquivo = new File(nome);
+            if (arquivo.createNewFile()) {
+                fileMenu.text("Arquivo criado: " + arquivo.getName(), 4);
+            } else {
+                fileMenu.text("O arquivo já existe.", 4);
+            }
+        } catch (IOException e) {
+            System.out.println("Ocorreu um erro ao criar o arquivo.");
+            e.printStackTrace();
+        }
+    }
+
 }
