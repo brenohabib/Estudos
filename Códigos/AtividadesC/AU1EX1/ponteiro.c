39afcae65f26f2775c835b55b2f74242c7b54ff1
brenohabib <brenohabib2013@gmail.com>
2024-07-08 11:54:15 -0300

    vault backup: 2024-07-08 11:54:14

--- Códigos/AtividadesC/AU1EX1/ponteiro.c
+++ Códigos/AtividadesC/AU1EX1/ponteiro.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 
-int main(){
+void printAge(int *pAge)
+{
+   printf("You are %d years old\n", *pAge); //dereference
+}
 
-    int age = 21;
-    int *pAge = &age;
+int main()
+{
+   // pointer = a "variable-like" reference that holds a memory address to another variable, array, etc.
+   //           some tasks are performed more easily with pointers
+   //           * = indirection operator (value at address)
 
-    printf("address of age: %p\n", &age);
-    printf("address of age: %p\n", pAge);
+   int age = 21;
+   int *pAge = &age;
 
-    
+   printAge(pAge);
 
+   //printf("address of age: %p\n", &age);
+   //printf("value of pAge: %p\n", pAge);
+   //printf("size of age: %d bytes\n", sizeof(age));
+   //printf("size of pAge: %d bytes\n", sizeof(pAge));
+   //printf("value of age: %d\n", age);
+   //printf("value at stored address: %d\n", *pAge); //dereferencing
 
-    printf("value of age %d\n", age);
-    printf("value at storaged address: %d\n", *pAge);
-
-    return 0;
+   return 0;
 }
