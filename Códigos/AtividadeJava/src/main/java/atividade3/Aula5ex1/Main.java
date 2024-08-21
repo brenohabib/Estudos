fda89a6220e0ed8c2cbdf399752ff6545807daa5
brenohabib <brenohabib2013@gmail.com>
2024-08-21 08:18:56 -0300

    vault backup: 2024-08-21 08:18:56

--- Códigos/AtividadeJava/src/main/java/atividade3/Aula5ex1/Main.java
+++ Códigos/AtividadeJava/src/main/java/atividade3/Aula5ex1/Main.java
@@ -8,35 +8,38 @@ public class Main {
         LocalDate finalDate = LocalDate.of(9999, 11, 29);
         LocalDate dataPagamento = LocalDate.of(2022, 1, 5);
 
-        Aluno breno = new Aluno("Breno Habib Dhaher Muhammad Oliveira",
-                                19,
-                                "Sem experiencia",
-                                "Nilson Veloso 1",
-                                0,
-                                "Vitalicio",
-                                initDate,
-                                finalDate
+        Aluno breno = new Aluno(
+                "Breno Habib Dhaher Muhammad Oliveira",
+                19,
+                "Sem experiencia",
+                "Nilson Veloso 1",
+                0,
+                "Vitalicio",
+                initDate,
+                finalDate
         );
-        Aula aulaDoGustavo = new Aula("Gustavo",
+        Aula aulaDoGustavo = new Aula(
+                "Gustavo",
                 40,
                 "Feminino",
                 "Pauzanes",
                 0,
                 "Dar aula"
         );
-        Aula aulaDoGustavo2 = new Aula("Gustavo",
+        Aula aulaDoGustavo2 = new Aula(
+                "Gustavo",
                 40,
                 "Feminino",
                 "Pauzanes",
                 1,
                 "Dar aula"
         );
-        Pagamento pagamento = new Pagamento(0,
-                                                     0,
-                                                     567.99,
-                                                     dataPagamento,
-                                                     "Vencido"
-
+        Pagamento pagamento = new Pagamento(
+                0,
+                0,
+                567.99,
+                dataPagamento,
+                "Vencido"
         );
         aulaDoGustavo.inscreverAluno(breno);
         aulaDoGustavo2.inscreverAluno(breno);
