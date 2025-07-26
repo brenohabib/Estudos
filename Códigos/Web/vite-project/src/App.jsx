31c455d10c0d3f9d9c35161d71f8622dc2933820
brenohabib <brenohabib2013@gmail.com>
2025-07-25 21:47:07 -0300

    vault backup: 2025-07-25 21:47:06

--- Códigos/Web/vite-project/src/App.jsx
+++ Códigos/Web/vite-project/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState(0)
 
   return (
     <>
@@ -18,9 +18,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <input onChange={() => setInput((input) => input)}>
+          count is {input}
+        </input>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
