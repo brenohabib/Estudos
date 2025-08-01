7edeac9a6e3c3131a8748bec812308bdd2d1e372
brenohabib <brenohabib2013@gmail.com>
2025-08-01 16:15:26 -0300

    vault backup: 2025-08-01 16:15:26

--- Códigos/Web/vite-project/src/components/FormInput.jsx
+++ Códigos/Web/vite-project/src/components/FormInput.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useState } from "react";
+import FormButton from "./FormButton";
 
 function FormInput({ onSubmit }) {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
 
   const handleInputChange = (e) => {
     setInput(e.target.value);
@@ -25,13 +26,7 @@ function FormInput({ onSubmit }) {
           placeholder=". . ."
         />
       </div>
-
-      <button
-        type="submit"
-        className="w-full bg-gradient-to-r from-blue-500 to-purple-600 text-white font-medium py-2 px-4 rounded-lg hover:opacity-90 transition-all duration-200 active:scale-95"
-      >
-        Enviar
-      </button>
+      <FormButton />
     </form>
   );
 }
