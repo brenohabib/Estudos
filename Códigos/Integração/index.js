c5abc5ea88e2661a48554a288ff5c3c348c7c89d
brenohabib <brenohabib2013@gmail.com>
2025-08-06 20:35:24 -0300

    vault backup: 2025-08-06 20:35:24

--- Códigos/Integração/index.js
+++ Códigos/Integração/index.js
@@ -1,9 +1,45 @@
 const express = require('express');
+
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.status(200).send('Hello World!');
+let usuario = {
+    id: 1,
+    nome: 'Breno',
+    email: 'breno@email.com',
+    senha: '99999999'
+}
+
+let currentId = 1;
+
+app.get('/usuarios', (req, res) => {
+    res.status(200).json(usuario);
+})
+
+app.post('/usuarios' , (req, res) => {
+    const { nome, email, senha } = req.body;
+
+    if (!nome || !email || !senha) {
+        return res.status(400).json({'message': 'Preencha todos os campos'})
+    }
+
+    currentId += 1
+    const newUsuario = {
+        id: currentId,
+        nome,
+        email,
+        senha
+    }
+    res.status(201).json(newUsuario)
+})
+
+app.patch('/usuarios/:id', (req, res) => {
+    const { nome, email, senha } = req.body;
+    const { id } = req.params;
+
+    if (id != usuario.id) {
+        return res.status(404).json({'message': 'Usuario não encontrado'})
+    }
 })
 
 app.listen(PORT, () => {
